c6e221bae921790fe12f7cc35eba4da40018851a
shijin <shijinpaleri@gmail.com>
2023-01-26 18:28:19 +0530

    Added swagger docs

---
+++ plugins/swagger.js
@@ -0,0 +1,56 @@
+import { readFileSync } from 'fs'
+import { join } from 'desm'
+import fp from 'fastify-plugin'
+import Swagger from '@fastify/swagger'
+import SwaggerUI from '@fastify/swagger-ui'
+
+const { version } = JSON.parse(readFileSync(join(import.meta.url, '../package.json')))
+
+async function swaggerGenerator (fastify, opts) {
+  // Swagger documentation generator for Fastify.
+  // It uses the schemas you declare in your routes to generate a swagger compliant doc.
+  // https://github.com/fastify/fastify-swagger
+  await fastify.register(Swagger, {
+    swagger: {
+      info: {
+        title: 'Fastify Template',
+        description: 'Fastify Template REST documentation',
+        version
+      },
+      externalDocs: {
+        url: 'https://github.com/shijin-p/rest-template',
+        description: 'Find more info here'
+      },
+      host: 'localhost', // and your deployed url
+      schemes: ['http', 'https'],
+      consumes: ['application/json'],
+      produces: ['application/json', 'text/html'],
+      securityDefinitions: {
+        Bearer: {
+          type: 'apiKey',
+          name: 'Bearer',
+          in: 'header'
+        },
+        Csrf: {
+          type: 'apiKey',
+          name: 'x-csrf-token',
+          in: 'header'
+        }
+      }
+    },
+    // let's expose the documentation only in development
+    // it's up to you decide who should see this page,
+    // but it's alwaysx better to start safe.
+    exposeRoute: fastify.config.NODE_ENV !== 'production'
+  })
+
+  if (fastify.config.NODE_ENV !== 'production') {
+    await fastify.register(SwaggerUI, {
+      routePrefix: '/documentation'
+    })
+  }
+}
+
+export default fp(swaggerGenerator, {
+  name: 'swaggerGenerator'
+})
